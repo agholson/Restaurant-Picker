fbea4b57e345791ca9cf80c36d7ad6ee96bbb907
agholson <87455899+agholson@users.noreply.github.com>
2021-08-17 20:36:40 -0400

    Initial Commit

---
+++ Challenge9PickerRestaurant/Challenge9PickerRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge9PickerRestaurantApp.swift
+//  Challenge9PickerRestaurant
+//
+//  Created by Shepherd on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge9PickerRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
